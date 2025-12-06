ecc238ae531132f1f4d101c444d4c9ca06465d76
irudhirn <86399454+irudhirn@users.noreply.github.com>
2025-12-06 16:23:12 +0530

    dotenv  updated

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config({
-//   path: ".env"
-// });
+dotenv.config({
+  path: ".env"
+});
 
 console.log("process.env.MONGO_COMPASS_URI", process.env.MONGO_COMPASS_URI);
 
